4f79cd3986ef21f62fe612f6b6e5315cb5b05fda
Onesiphorus <pbagley77@gmail.com>
2018-05-22 15:45:03 -0500

    update

---
+++ SimpleStorageApp/app/src/main/java/com/a5402technologies/pbagley/simplestorageapp/ItemBase.java
@@ -0,0 +1,145 @@
+package com.a5402technologies.pbagley.simplestorageapp;
+
+import java.util.List;
+
+public class ItemBase {
+    private String name;
+    private Integer cost;
+    private Integer sell;
+    private Integer weight;
+    private Integer darkStone;
+    private Integer agilityBonus;
+    private Integer cunningBonus;
+    private Integer spiritBonus;
+    private Integer strengthBonus;
+    private Integer loreBonus;
+    private Integer luckBonus;
+    private Integer healthBonus;
+    private Integer sanityBonus;
+    private Integer initiativeBonus;
+    private List<String> restrictions;
+
+    public ItemBase(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Integer getSell() {
+        return sell;
+    }
+
+    public void setSell(Integer sell) {
+        this.sell = sell;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getDarkStone() {
+        return darkStone;
+    }
+
+    public void setDarkStone(Integer darkStone) {
+        this.darkStone = darkStone;
+    }
+
+    public Integer getAgilityBonus() {
+        return agilityBonus;
+    }
+
+    public void setAgilityBonus(Integer agilityBonus) {
+        this.agilityBonus = agilityBonus;
+    }
+
+    public Integer getCunningBonus() {
+        return cunningBonus;
+    }
+
+    public void setCunningBonus(Integer cunningBonus) {
+        this.cunningBonus = cunningBonus;
+    }
+
+    public Integer getSpiritBonus() {
+        return spiritBonus;
+    }
+
+    public void setSpiritBonus(Integer spiritBonus) {
+        this.spiritBonus = spiritBonus;
+    }
+
+    public Integer getStrengthBonus() {
+        return strengthBonus;
+    }
+
+    public void setStrengthBonus(Integer strengthBonus) {
+        this.strengthBonus = strengthBonus;
+    }
+
+    public Integer getLoreBonus() {
+        return loreBonus;
+    }
+
+    public void setLoreBonus(Integer loreBonus) {
+        this.loreBonus = loreBonus;
+    }
+
+    public Integer getLuckBonus() {
+        return luckBonus;
+    }
+
+    public void setLuckBonus(Integer luckBonus) {
+        this.luckBonus = luckBonus;
+    }
+
+    public Integer getHealthBonus() {
+        return healthBonus;
+    }
+
+    public void setHealthBonus(Integer healthBonus) {
+        this.healthBonus = healthBonus;
+    }
+
+    public Integer getSanityBonus() {
+        return sanityBonus;
+    }
+
+    public void setSanityBonus(Integer sanityBonus) {
+        this.sanityBonus = sanityBonus;
+    }
+
+    public Integer getInitiativeBonus() {
+        return initiativeBonus;
+    }
+
+    public void setInitiativeBonus(Integer initiativeBonus) {
+        this.initiativeBonus = initiativeBonus;
+    }
+
+    public List<String> getRestrictions() {
+        return restrictions;
+    }
+
+    public void setRestrictions(List<String> restrictions) {
+        this.restrictions = restrictions;
+    }
+}
---
+++ SimpleStorageApp/app/src/main/java/com/a5402technologies/pbagley/simplestorageapp/RangedWeapon.java
@@ -0,0 +1,12 @@
+package com.a5402technologies.pbagley.simplestorageapp;
+
+public class RangedWeapon extends ItemBase {
+    private Integer damageDie;
+    private Integer damageBonus;
+    private Integer shots;
+    private Integer range;
+
+    public RangedWeapon(String name) {
+        super(name);
+    }
+}

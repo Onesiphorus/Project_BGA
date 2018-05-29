8f7a46fbfe66fdc85e0ed72005f478ccf7701160
Onesiphorus <pbagley77@gmail.com>
2018-05-29 14:19:49 -0500

    activities added

---
+++ SimpleStorageApp/app/src/main/java/com/a5402technologies/pbagley/simplestorageapp/Gear/ClothingActivity.java
@@ -0,0 +1,15 @@
+package com.a5402technologies.pbagley.simplestorageapp.Gear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.a5402technologies.pbagley.simplestorageapp.R;
+
+public class ClothingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing);
+    }
+}
